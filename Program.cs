3e4667001482859c3f7e12108bc24ec7622bfb78
Coco <143611879+cfrikke@users.noreply.github.com>
2024-01-09 13:41:46 +0000

    OwO

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Owoify;
+public class Program {
+    public static void Main(){
+        Console.WriteLine(Owoifier.Owoify("This is the string to owo! Kinda cute isn't it?", Owoifier.OwoifyLevel.Uvu));
+        Console.WriteLine(Owoifier.Owoify("This is the string to owo! Kinda cute isn't it?", Owoifier.OwoifyLevel.Uvu));
+        Console.WriteLine(Owoifier.Owoify("This is the string to owo! Kinda cute isn't it?", Owoifier.OwoifyLevel.Uvu));
+        Console.WriteLine(Owoifier.Owoify("This is the string to owo! Kinda cute isn't it?", Owoifier.OwoifyLevel.Uvu));
+        Console.WriteLine(Owoifier.Owoify("This is the string to owo! Kinda cute isn't it?", Owoifier.OwoifyLevel.Uvu));
+        Console.WriteLine(Owoifier.Owoify("This is the string to owo! Kinda cute isn't it?", Owoifier.OwoifyLevel.Uvu));
+
+    }
+}
